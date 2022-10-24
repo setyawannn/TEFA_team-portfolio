34e62355061ed7c3123de3b25e0a7a06faee049c
setyawannn <prayogasetyawan.adi@gmail.com>
2022-10-24 07:44:48 +0700

    Add: smooth scrollbar

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,14 @@ const nav=document.querySelector('nav ul');
 menuToggle.addEventListener('click', function(){
     nav.classList.toggle('slide');
 });
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+  });
+  
